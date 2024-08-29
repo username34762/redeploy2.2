2e7b0889a81055ffee86849ddaa1c3e53a01afb2
NayelixFab <141381204+rocs2025@users.noreply.github.com>
2024-08-28 21:08:33 -0600

    changes

---
+++ app/(routes)/(home)/places/[id]/PlaceClientComponent.tsx
@@ -0,0 +1,128 @@
+"use client";
+import Image from 'next/image';
+import { FaStar } from 'react-icons/fa';
+import { Button } from '@nextui-org/button';
+import { MapPin, Heart } from 'lucide-react';
+import { NavBar } from '@/components/Shared/NavBar';
+import Link from "next/link";
+import { useLovedPlaces } from "@/hooks/use-loved-places";
+import { useAuth } from "@clerk/nextjs";
+import { Place } from "@prisma/client";
+
+export default function PlaceClientComponent({ place }: { place: Place }) {
+  const { addLovedItem, lovedItems, removeLovedItem } = useLovedPlaces();
+  const likedPlace = lovedItems.some((item) => item.id === place.id);
+  
+  const availability = [
+    "Public Restrooms",
+    "Shops or Kiosks",
+    "Trash Cans or Waste Containers",
+    "Picnic Areas",
+    "Camping Zones",
+    "Hiking Trails",
+    "Viewpoints",
+    "Parking Lots",
+    "Information Booths",
+    "Fishing Areas",
+    "Swimming Areas",
+  ];
+
+  const hours = {
+    Monday: "8:00 AM - 5:00 PM",
+    Tuesday: "8:00 AM - 6:00 PM",
+    Wednesday: "Closed",
+    Thursday: "8:00 AM - 6:00 PM",
+    Friday: "8:00 AM - 6:00 PM",
+    Saturday: "8:00 AM - 4:00 PM",
+    Sunday: "8:00 AM - 6:00 PM",
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div className="container mx-auto px-6 py-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-10 ">
+          <div className="relative col-span-2 h-[300px] md:h-[400px]">
+            <Image
+              src={place.photo}
+              alt={place.name}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
+            />
+            <div className="absolute top-4 right-4">
+              <button className="bg-white text-black py-1 px-3 rounded-lg shadow-md">ALL PHOTOS</button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="relative h-[145px] md:h-[195px]">
+              <Image
+                src={place.photo}
+                alt="Secondary Image 1"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+            <div className="relative h-[145px] md:h-[195px]">
+              <Image
+                src={place.photo}
+                alt="Secondary Image 2"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="md:col-span-2">
+            <h1 className="text-4xl font-semibold mb-4">{place.name}</h1>
+            <div className="flex items-center mb-6">
+              <FaStar className="text-yellow-500 mr-2" />
+              <span className="text-xl font-semibold">{place.rating}</span>
+              <span className="text-gray-500 ml-2">(18 Reviews)</span>
+            </div>
+            <h2 className="text-2xl font-semibold mb-6">About</h2>
+            <p className="text-gray-700 leading-relaxed mb-10">{place.description}</p>
+            <h3 className="text-xl font-semibold mb-4">Amenities</h3>
+            <ul className="list-disc ml-4 text-gray-700 space-y-2">
+              {availability.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="border rounded-lg p-6 shadow-md">
+  <div className="flex items-center justify-between mb-4">
+    <h3 className="text-xl font-semibold">Location</h3>
+    <Heart
+      className={`cursor-pointer ${likedPlace && "fill-black"}`}
+      onClick={likedPlace ? () => removeLovedItem(place.id) : () => addLovedItem(place)}
+    />
+  </div>
+  <p className="flex items-center mb-6">
+    <MapPin className="h-4 w-4 mr-2" strokeWidth={1} />
+    {place.ubication}
+  </p>
+  
+  <Button className="w-full py-2 rounded-3xl mt-6 bg-[#4fb350]" variant="ghost">
+    GO
+  </Button>
+  
+  <h3 className="text-xl font-semibold mt-10 mb-4">Hours</h3>
+  <ul className="text-gray-700 space-y-2">
+    {Object.entries(hours).map(([day, hours], index) => (
+      <li key={index}>
+        <span className="font-semibold">{day}:</span> {hours}
+      </li>
+    ))}
+  </ul>
+  
+  <h3 className="text-bold text-3xl text-center mt-8 text-[#9747FF]">Tours coming soon!</h3>
+</div>
+
+        </div>
+      </div>
+    </>
+  );
+}
--- app/(routes)/(home)/places/components/ListPlaces/ListPlaces.tsx
+++ app/(routes)/(home)/places/components/ListPlaces/ListPlaces.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { Button } from "@/components/ui/button";
 import { Place } from "@prisma/client";
 import Image from "next/image";
@@ -11,65 +10,76 @@ import { useAuth } from "@clerk/nextjs";
 import { SkeletonPlaces } from "@/components/Shared/SkeletonPlaces";
 
 export function ListPlaces(props: ListPlacesProps) {
-    const { places } = props;
-    const { userId } = useAuth();
-    const { addLovedItem, lovedItems, removeLovedItem } = useLovedPlaces();
+  const { places } = props;
+  const { userId } = useAuth();
+  const { addLovedItem, lovedItems, removeLovedItem } = useLovedPlaces();
 
-    if (!places) {
-        return <SkeletonPlaces />
-    }
+  if (!places) {
+    return <SkeletonPlaces />;
+  }
 
-    return (
-        <>
-            {places.length === 0 && (
-                <p>Places not found</p>
-            )}
-            <div className="grid md:grid-cols-2 gap-6 lg:grid-cols-4">
-                {places.map((place: Place) => {
-                    const { photo, id, name, description, rating, ubication } = place;
-                    const likedPlace = lovedItems.some((item) => item.id === place.id);
+  return (
+    <>
+      {places.length === 0 && <p>Places not found</p>}
+      <div className="grid md:grid-cols-2 gap-6 lg:grid-cols-4">
+        {places.map((place: Place) => {
+          const { photo, id, name, description, rating, ubication } = place;
+          const likedPlace = lovedItems.some((item) => item.id === place.id);
 
-                    return (
-                        <div key={id} className="p-1 rounded-3xl shadow-md hover:shadow-lg">
-                            {/* Envolvemos la imagen y el nombre del lugar con Link */}
-                            <Link href={`/places/${id}`}>
-                                <Image src={photo} alt={name} width={400} height={600} className="rounded-3xl" />
-                                <div className="p-3">
-                                    <div className="flex flex-col mb-3 gap-x-4">
-                                        <p className="text-xl min-h-16 lg:min-h-fit">{name}</p>
-                                        <p>{description}</p>
-                                    </div>
-                                    <p className="flex items-center">
-                                        <MapPin className="h-4 w-4 mr-2" strokeWidth={1} />
-                                        {ubication}
-                                    </p>
-                                </div>
-                            </Link>
-                            <div className="flex items-center justify-between px-3">
-                                <div className="flex items-center text-yellow-500">
-                                    <span className="text-lg font-semibold">{rating}</span>
-                                    <FaStar className="ml-1" />
-                                </div>
-                               <Link href={`/places/${id}`}>
-                                <button className="flex items-center justify-center bg-green-500 text-white px-3 py-1 rounded-lg text-sm font-semibold">
-                                    GO <FaSuitcase className="ml-1" /> 
-                                </button>
-                                </Link>
-                               
-                                {userId ? (
-                                    <Heart className={`mt-2 cursor-pointer ${likedPlace && "fill-black"}`} onClick={likedPlace ? () => removeLovedItem(place.id) : () => addLovedItem(place)} />
-                                ) : (
-                                    <div className="w-full mt-2 text-center">
-                                        <Link href="/sign-up">
-                                            <Button variant="outline">Sign Up</Button>
-                                        </Link>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    )
-                })}
+          return (
+            <div key={id} className="p-1 rounded-3xl shadow-md hover:shadow-lg">
+              <Link href={`/places/${id}`}>
+                <Image src={photo} alt={name} width={400} height={600} className="rounded-3xl" />
+                <div className="p-3">
+                  <div className="flex flex-col mb-3 gap-x-4">
+                    <p className="text-xl min-h-16 lg:min-h-fit">{name}</p>
+                    
+                  </div>
+                  <p className="flex items-center">
+                    <MapPin className="h-4 w-4 mr-2" strokeWidth={1} />
+                    {ubication}
+                  </p>
+                </div>
+              </Link>
+              <div className="flex flex-col px-3">
+                
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center text-yellow-500">
+                    <span className="text-lg font-semibold">{rating}</span>
+                    <FaStar className="ml-1" />
+                  </div>
+                  <Link href={`/places/${id}`}>
+                    <button
+                      className="flex items-center justify-center bg-green-500 text-white px-3 py-1 rounded-lg text-sm font-semibold"
+                    >
+                      GO <FaSuitcase className="ml-1" />
+                    </button>
+                  </Link>
+                </div>
+                
+               
+                {userId && (
+                  <div className="mt-2 self-end">
+                    <Heart
+                      className={`cursor-pointer ${likedPlace && "fill-black"}`}
+                      onClick={likedPlace ? () => removeLovedItem(place.id) : () => addLovedItem(place)}
+                    />
+                  </div>
+                )}
+
+                
+                {!userId && (
+                  <div className="mt-4 text-center">
+                    <Link href="/sign-up">
+                      <Button variant="outline" className="w-full rounded-3xl">Sign Up</Button>
+                    </Link>
+                  </div>
+                )}
+              </div>
             </div>
-        </>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 }
