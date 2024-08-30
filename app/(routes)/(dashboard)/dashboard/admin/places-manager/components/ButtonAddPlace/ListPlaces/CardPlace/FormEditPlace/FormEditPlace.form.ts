654fb4c44ceae28fe72826dbd791fe99993b6d3f
NayelixFab <141381204+rocs2025@users.noreply.github.com>
2024-08-29 19:33:55 -0600

    new

--- app/(routes)/(dashboard)/dashboard/admin/places-manager/components/ButtonAddPlace/ListPlaces/CardPlace/FormEditPlace/FormEditPlace.form.ts
+++ app/(routes)/(dashboard)/dashboard/admin/places-manager/components/ButtonAddPlace/ListPlaces/CardPlace/FormEditPlace/FormEditPlace.form.ts
@@ -1,8 +1,8 @@
 import {z } from "zod";
 export const formSchema = z.object({
-    name: z.string().min(2).max(50),
-    description: z.string().min(2).max(50),
-    ubication: z.string().min(2).max(50),
+    name: z.string().min(2).max(250),
+    description: z.string().min(2).max(250),
+    ubication: z.string().min(2).max(250),
     rating: z.string().min(2).max(50),
     photo: z.string().min(1).max(100),
     isPublish: z.boolean(),
